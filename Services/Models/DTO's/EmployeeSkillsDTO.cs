26ceae67f138f4400aaad54690126c3ce1726c67
Ruchira, Dhanasri <Dhanasri.Ruchira@evicore.com>
2025-06-16 14:44:06 +0530

    Certification  category removal

--- Services/Models/DTO's/EmployeeSkillsDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EviHub.Models.DTO_s
-{
-    public class EmployeeSkillsDTO
-    {
-        public int EmpSkillId { get; set; }
-        public int EmpId { get; set; }
-        public int SkillId { get; set; }
-    }
-}
